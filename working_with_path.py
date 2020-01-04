d1eb4dfc131365d99c73287c348ad5ee5c171893
Apple <apple@Apples-MacBook-Air.local>
2020-01-04 20:48:00 +0530

    Modified working_with_path.py

--- working_with_path.py
+++ working_with_path.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-path = Path("/Users/apple/Desktop/datastructure/data.py")
+path = Path("/Users/apple/Desktop/datastructure")
 print(path.exists())
 print(path.is_file())
 print(path.is_dir())
@@ -8,6 +8,11 @@ print(path.name)
 print(path.stem)
 print(path.suffix)
 print(path.parent)
+
+paths = [p for p in path.iterdir() if p.is_dir()]
+py_files = [p for p in path.glob("*.py")]
+print(paths)
+print(py_files)
 
 path = path.with_suffix(".py")
 print(path)
