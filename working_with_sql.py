a5b492d18b5ce3eae50076dda7c495b28d7bd056
Apple <apple@Apples-MacBook-Air.local>
2020-01-04 23:19:01 +0530

    working with sql

---
+++ working_with_sql.py
@@ -0,0 +1,17 @@
+import sqlite3
+from pathlib import Path
+import json
+
+# movies = json.loads(Path("movies.json").read_text())
+# with sqlite3.connect("db.sqlite3") as conn:
+#     command = "INSERT INTO Movies VALUES(?,?,?)"
+#     for movie in movies:
+#         conn.execute(command, tuple(movie.values()))
+#     conn.commit()
+
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "SELECT * FROM Movies"
+    cursor = conn.execute(command)
+    # for row in cursor:
+    #     print(row)
+    print(cursor.fetchall())
