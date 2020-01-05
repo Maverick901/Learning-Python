3a34ec33e5a24ba4a2bf171547ef7562558575f0
Apple <apple@Apples-MacBook-Air.local>
2020-01-05 14:05:53 +0530

    working with timedelta

--- working_with_datetime.py
+++ working_with_datetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 print(datetime(2019, 12, 2))
@@ -6,3 +6,17 @@ print(datetime.now())
 print(datetime.fromtimestamp(time.time()))
 dt = datetime.now()
 print(dt.strftime("%Y/%m/%d"))
+
+dt1 = datetime(2011, 11, 11)
+dt2 = datetime.now()
+
+duration = dt2 - dt1
+print(duration)
+print(duration.days)
+print(duration.seconds)
+
+dt2 += timedelta(days=1, seconds=10000)
+duration = dt2 - dt1
+print(duration)
+print(duration.days)
+print(duration.seconds)
